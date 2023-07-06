a9fe2e85ce1949c0244cf732a33f466db5935461
JulianaMcarmo <128004267+JulianaMcarmo@users.noreply.github.com>
2023-07-05 21:04:11 -0300

    Add files via upload

--- main.r
+++ main.r
@@ -13,3 +13,19 @@ nomes_pokemons_filtrados <- pokemons_filtrados$name
 # Exibir os nomes dos pokemons filtrados
 print(nomes_pokemons_filtrados)
 
+media_hp <- mean(pokemons_filtrados$hp)
+desvio_padrao_hp <- sd(pokemons_filtrados$hp)
+
+# Criar um vetor para armazenar os Z-scores
+z_scores <- vector("numeric", length = nrow(pokemons_filtrados))
+
+# Calcular o Z-score para cada Pokémon filtrado
+for (i in 1:nrow(pokemons_filtrados)) {
+  z_scores[i] <- (pokemons_filtrados$hp[i] - media_hp) / desvio_padrao_hp
+  
+  # Combinar o nome do Pokémon com o Z-score correspondente
+  resultado <- paste("Pokémon:", pokemons_filtrados$nomes[i], "| Z-Score:", z_scores[i])
+  
+  # Exibir o resultado
+  print(resultado)
+}
